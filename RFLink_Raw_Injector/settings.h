a98f051884cba8c607cf864aa2eba798741a8fb8
couin3 <35873376+couin3@users.noreply.github.com>
2020-04-08 18:38:01 +0200

    Changed Default settings

--- RFLink_Raw_Injector/settings.h
+++ RFLink_Raw_Injector/settings.h
@@ -1,6 +1,6 @@
-// #define PIN_GND 2
-// #define PIN_VCC 3
-#define PIN_DATA 4
+// #define PIN_GND NOT_A_PIN
+// #define PIN_VCC NOT_A_PIN
+#define PIN_DATA 2
 const byte msgRepeat = 3;
 
 #define NELEMS(x) (sizeof(x) / sizeof((x)[0]))
